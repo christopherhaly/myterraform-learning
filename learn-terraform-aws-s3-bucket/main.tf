f181ad5c5addb384d1245686e8e2e01d7fff5895
christopherhaly <christopherhaly@users.noreply.github.com>
2021-05-29 20:43:29 -0400

    added subcription for sqs to sns topic

--- learn-terraform-aws-s3-bucket/main.tf
+++ learn-terraform-aws-s3-bucket/main.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket-ccn-1"
-  acl    = "private"
+  bucket        = "my-tf-test-bucket-ccn-1"
+  acl           = "private"
   force_destroy = true
 
   tags = {
@@ -46,6 +46,45 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
   topic {
     topic_arn     = aws_sns_topic.topic.arn
     events        = ["s3:ObjectCreated:*"]
-    filter_suffix = ".log"
+    filter_suffix = ".csv"
   }
+}
+
+resource "aws_sqs_queue" "terraform_queue" {
+  name                      = "terraform-example-queue"
+
+  tags = {
+    Environment = "development"
+  }
+}
+
+resource "aws_sqs_queue_policy" "test" {
+  queue_url = aws_sqs_queue.terraform_queue.id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "sqspolicy",
+  "Statement": [
+    {
+      "Sid": "First",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.terraform_queue.arn}",
+      "Condition": {
+        "ArnEquals": {
+          "aws:SourceArn": "${aws_sns_topic.topic.arn}"
+        }
+      }
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
+  topic_arn = aws_sns_topic.topic.arn
+  protocol  = "sqs"
+  endpoint  = aws_sqs_queue.terraform_queue.arn
 }
