d96cc43bef4521a1b9b0bd9c82a2ecb8b4b79fb0
christopherhaly <christopherhaly@users.noreply.github.com>
2021-05-30 07:35:09 -0400

    initial

---
+++ learn-terraform-aws-vpc/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_vpc" "development_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+
